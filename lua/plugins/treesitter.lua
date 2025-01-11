33d31baf6f2914522c4d22f1d3df05992540d108
Nimesh Srivastava <snimesh412@gmail.com>
2025-01-12 04:23:55 +0530

    Config update

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -2,10 +2,10 @@ return {
 	"nvim-treesitter/nvim-treesitter",
 	build = ":TSUpdate",
 	config = function()
-		require("nvim-treesitter.install").compilers = { "zig" , "clang", "gcc"}
+		require("nvim-treesitter.install").compilers = { "zig" }
 		local config = require("nvim-treesitter.configs")
 		config.setup({
-            ensure_installed = {"c", "python", "rust"},
+			ensure_installed = { "c", "python", "rust" },
 			auto_install = true,
 			highlight = { enable = true },
 			indent = { enable = true },
