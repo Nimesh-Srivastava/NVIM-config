012d47be5a0a665d1c59a71978584b604025208a
Nimesh Srivastava <snimesh412@gmail.com>
2025-01-12 05:39:11 +0530

    Added format on save

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -23,6 +23,23 @@ return {
 			lspconfig.ts_ls.setup({ capabilities = capabilities })
 			lspconfig.pylsp.setup({ capabilities = capabilities })
 			lspconfig.tailwindcss.setup({ capabilities = capabilities })
+
+            vim.api.nvim_create_autocmd('LspAttach', {
+                callback = function (args)
+                    local client = vim.lsp.get_client_by_id(args.data.client_id)
+                    if not client then return end
+                    if client.supports_method('textDocument/formatting') then
+                        vim.api.nvim_create_autocmd('BufWritePre', {
+                            buffer = args.buf,
+                            callback = function ()
+                                vim.lsp.buf.format({bufnr = args.buf, id = client.id})
+                            end
+                        })
+                    end
+
+                end
+            })
+
 			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
 			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
 			vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
