773c6a2f5ce2685b53023fef29fef3cddaac7b52
n3u <snimesh412@gmail.com>
2026-01-12 11:19:01 +0530

    Refactor none-ls configuration and keymap setup

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -1,13 +1,22 @@
 return {
-	"nvimtools/none-ls.nvim",
-	config = function()
-		local null_ls = require("null-ls")
-		null_ls.setup({
-			sources = {
-				null_ls.builtins.formatting.stylua,
-				null_ls.builtins.formatting.prettier,
-			},
-		})
-		vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
-	end,
+    "nvimtools/none-ls.nvim",
+    config = function()
+        local none_ls = require("null-ls")
+
+        none_ls.setup({
+            sources = {
+                none_ls.builtins.formatting.prettier,
+                none_ls.builtins.formatting.stylua,
+            },
+        })
+
+        vim.keymap.set("n", "<leader>gf", function()
+            vim.lsp.buf.format({
+
+                filter = function(client)
+                    return client.name == "none-ls"
+                end,
+            })
+        end, { desc = "Format buffer" })
+    end,
 }
